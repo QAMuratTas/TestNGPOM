82db71138ef6b830e0128076c6797b45663137f3
QA Murat Tas <itmmt38@gmail.com>
2023-01-06 22:28:30 +0300

    friday

--- src/test/java/murat/tests/Day02_C04_PageClassFindAll.java
+++ src/test/java/murat/tests/Day02_C04_PageClassFindAll.java
@@ -1,8 +1,22 @@
 package murat.tests;
 
+import murat.pages.SauceDemoLoginPage;
+import murat.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Day02_C04_PageClassFindAll {
 
 
+    @Test
+    public void test(){
+
+        Driver.getDriver().get("https://www.saucedemo.com/");
+
+        SauceDemoLoginPage lp = new SauceDemoLoginPage();
+
+        Assert.assertEquals(3, lp.inputElements.size());
+    }
 
 
 }
