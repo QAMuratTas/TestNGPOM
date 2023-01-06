6908e6fa19b867b988865bf3d83632f3eeb5edae
QA Murat Tas <itmmt38@gmail.com>
2023-01-06 20:48:45 +0300

    friday

--- src/test/java/murat/utilities/Driver.java
+++ src/test/java/murat/utilities/Driver.java
@@ -3,6 +3,7 @@ package murat.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
@@ -12,12 +13,26 @@ public class Driver {
 
     public static WebDriver getDriver(){
 
+          String browser=ConfigReader.getProperty("browser");
+
         if (driver== null){
-            WebDriverManager.chromedriver().setup();
-            driver=new ChromeDriver();
+            switch (browser) {
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+            }
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        }
+
+
+            }
 
 return driver;
     }
